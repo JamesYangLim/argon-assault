4cb34dcdc96d52399bc10a695ddb4d0c51182dec
James Lim <lim.yangkuang@gmail.com>
2020-10-25 22:49:06 +0800

    Created Level 1 and splash scenes

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] float SecondsToLoadNextScene = 3f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (SecondsToLoadNextScene < Time.time)
+        {
+            LoadNextScene();
+        }
+    }
+
+    void LoadNextScene()
+    {
+        var nextSceneBuildIndex = SceneManager.sceneCountInBuildSettings + 1;
+        SceneManager.LoadScene(nextSceneBuildIndex);
+    }
+}
