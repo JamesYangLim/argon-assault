7a816b3fa32d83bf9474f7e7e2196a4ffe29add1
James Lim <lim.yangkuang@gmail.com>
2020-10-27 08:19:15 +0800

    Added standard assets. Implement waypoints.

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer: MonoBehaviour
+{
+    [SerializeField] float SecondsToLoadNextScene = 3f;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        Invoke(nameof(LoadNextScene), SecondsToLoadNextScene);
+    }
+
+    void LoadNextScene()
+    {
+        //var nextSceneBuildIndex = SceneManager.sceneCountInBuildSettings + 1;
+        SceneManager.LoadScene(1);
+    }
+}
